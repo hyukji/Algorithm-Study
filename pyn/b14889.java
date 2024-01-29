6a4b9dd70010aeef2de0cb657335c93b00773b3f
FreshSangchoo <aprldpdy289@naver.com>
2024-01-29 20:56:31 +0900

    0129 "스타트 링크 구현 계획"

---
+++ pyn/b14889.java
@@ -0,0 +1,24 @@
+
+import.java.io.*;
+import java.util.*;
+
+public class b14889 {
+	public static void main(String[] args) {
+		//입력
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		
+		//Sij와 Sji의 합을 담은 배열 status 생성
+		
+		//NC(N/2)를 선택
+		
+		//선택한 집합의 원소로 만들 수 있는 모든 Permutation 작성
+		
+		// Permutation한 것에 해당되는 모든 Sij를 status에서 찾아서 누적
+		
+		// 두 집합을 비교
+		
+		// 모든 경우의 수를 따진 후 최솟값 출력
+	}
+
+}
