529990d608b6f71ebb43b688e35330594bb23244
kwlgur0416 <kwlgur0416@gmail.com>
2024-01-29 14:01:35 +0900

    BOJ_14503_로봇청소기.java 구현

--- hyukji/_240129/BOJ_14503_로봇청소기.java
+++ hyukji/_240129/BOJ_14503_로봇청소기.java
@@ -30,28 +30,29 @@ public class BOJ_14503_로봇청소기 {
 			}
 		}
 
+		// 청소된 부분 = 2
 		int answer = 0;
 		while(true) {
-			if(graph[r][c] == 0) {
+			if(graph[r][c] == 0) { // 1번
 				graph[r][c] = 2;
 				answer++;
 			}
 			
 			boolean hasBlank = false;
 			for(int i = 0; i < 4; i++) { 
-				d = (d+3) % 4;
+				d = (d+3) % 4; // 반시계방향으로 체크
 				int nr = r + dr[d];
 				int nc = c + dc[d];
-				if(graph[nr][nc] == 0) {
+				if(graph[nr][nc] == 0) { // 비어있다면 이동!
 					r = nr; c = nc;
 					hasBlank = true;
 					break;
 				}
 			}
 			
-			if(!hasBlank) {
+			if(!hasBlank) { // 4방향 확인 후 청소할 부부니 없다면, 후진!
 				r -= dr[d]; c -= dc[d]; 
-				if(graph[r][c] == 1) { 
+				if(graph[r][c] == 1) { // 후진 후 벽인것 체크
 					System.out.println(answer);
 					return;
 				}
